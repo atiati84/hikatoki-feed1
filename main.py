f689a35375d0c2a43a551b53f111f7ca047fee98
atiati84 <rinhasuhasu@gmail.com>
2025-12-25 21:48:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello Railway! Python is running.")
+import os
+from atproto import Client
+
+handle = os.environ.get("BSKY_HANDLE")
+password = os.environ.get("BSKY_APP_PASSWORD")
+
+client = Client()
+client.login(handle, password)
+
+print("ログイン成功")
+
+res = client.app.bsky.feed.search_posts(
+    q="ヒカトキ",
+    limit=5
+)
+
+for post in res.posts:
+    text = post.record.text.replace("\n", " ")
+    print("-", text)
